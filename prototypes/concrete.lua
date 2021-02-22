2bfa45eee616ad5c0ffb9e85e4242ee28ea74fff
InaccurateTank <51210312+InaccurateTank@users.noreply.github.com>
2021-02-22 00:57:46 -0800

    Ready for Release

--- prototypes/concrete.lua
+++ prototypes/concrete.lua
@@ -6,7 +6,7 @@ local ctypes = {
     "safety-concrete"
 }
 
--- Injection
+-- Game Injection
 local dir = {
     {this = "left", next = "right"},
     {this = "right", next = "left"}
@@ -19,14 +19,16 @@ for i = 1, #ctypes, 1 do
         sta.name = ctypes[i] .. "-" .. dir[d].this
         sta.next_direction = ctypes[i] .. "-" .. dir[d].next
         sta.minable.result = ctypes[i]
-        sta.variants.material_background.picture = "__hazard-stripes__/graphics/ops-concrete/ops-concrete-" .. dir[d].this .. ".png"
-        sta.variants.material_background.hr_version.picture = "__hazard-stripes__/graphics/ops-concrete/hr-ops-concrete-" .. dir[d].this .. ".png"
+        sta.variants.material_background.picture = "__hazard-stripes__/graphics/" .. ctypes[i] .. "/" .. ctypes[i] .. "-" .. dir[d].this .. ".png"
+        sta.variants.material_background.hr_version.picture = "__hazard-stripes__/graphics/" .. ctypes[i] .. "/hr-" .. ctypes[i] .. "-" .. dir[d].this .. ".png"
 
         -- Refined
         local ref = table.deepcopy(data.raw["tile"]["refined-hazard-concrete-" .. dir[d].this])
         ref.name = "refined-" .. ctypes[i] .. "-" .. dir[d].this
         ref.next_direction = "refined-" .. ctypes[i] .. "-" .. dir[d].next
         ref.minable.result = "refined-" .. ctypes[i]
+        ref.variants.material_background.picture = "__hazard-stripes__/graphics/" .. ctypes[i] .. "/refined-" .. ctypes[i] .. "-" .. dir[d].this .. ".png"
+        ref.variants.material_background.hr_version.picture = "__hazard-stripes__/graphics/" .. ctypes[i] .. "/hr-refined-" .. ctypes[i] .. "-" .. dir[d].this .. ".png"
 
         data:extend({sta, ref})
     end
@@ -46,6 +48,7 @@ for i = 1, #ctypes, 1 do
     -- Standard
     local staItem = table.deepcopy(data.raw["item"]["hazard-concrete"])
     staItem.name = ctypes[i]
+    staItem.icon = "__hazard-stripes__/graphics/icons/" .. ctypes[i] .. ".png"
     staItem.order = "b[concrete]-b["..ctypes[i].."]"
     staItem.place_as_tile =
     {
@@ -57,6 +60,7 @@ for i = 1, #ctypes, 1 do
     -- Refined
     local refItem = table.deepcopy(data.raw["item"]["refined-hazard-concrete"])
     refItem.name = "refined-" .. ctypes[i]
+    refItem.icon = "__hazard-stripes__/graphics/icons/refined-" .. ctypes[i] .. ".png"
     refItem.order = "b[concrete]-d[refined-".. ctypes[i] .. "]"
     refItem.place_as_tile =
     {
@@ -64,7 +68,7 @@ for i = 1, #ctypes, 1 do
         condition_size = 1,
         condition = { "water-tile" }
     }
-    
+
     data:extend({staRec, refRec, staItem, refItem})
     table.insert(data.raw.technology["concrete"].effects,{type = "unlock-recipe", recipe = ctypes[i]})
     table.insert(data.raw.technology["concrete"].effects,{type = "unlock-recipe", recipe = 'refined-' .. ctypes[i]})
