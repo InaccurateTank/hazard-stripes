2f233da189328befae135b999342888f73b0c7d3
InaccurateTank <inaccuratetank@outlook.com>
2024-03-10 19:48:25 -0700

    added migration, modern workflow, github action

---
+++ migrations/enable_recipes.lua
@@ -0,0 +1,18 @@
+for _, force in pairs(game.forces) do
+    local technologies = force.technologies
+    local recipes = force.recipes
+
+    if technologies["concrete"].researched then
+        local ctypes = {
+            "emergency-concrete",
+            "operations-concrete",
+            "radiation-concrete",
+            "safety-concrete"
+        }
+        for _, type in pairs(ctypes) do
+            local ref = 'refined-' .. type
+            recipes[type].enabled = true
+            recipes[ref].enabled = true
+        end
+    end
+end
